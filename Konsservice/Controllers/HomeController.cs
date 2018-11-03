b943fce07d7047fdcf6c55ea747f58f4973723dd
Dima <musiienko1999@gmail.com>
2018-11-03 19:54:22 +0200

    First commit

---
+++ Konsservice/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Konsservice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ContactsView()
+        {
+            return View();
+        }
+
+        public ActionResult ServiceView()
+        {
+            return View();
+        }
+
+        public ActionResult ReportsView()
+        {
+            return View();
+        }
+
+        public ActionResult ClientsView()
+        {
+            return View();
+        }
+
+        public ActionResult SiteMapView()
+        {
+            return View();
+        }
+    }
+}
